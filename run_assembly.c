b6c05c0e07d598d47d5f3112bfb86102f942ea59
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-16 00:33:25 -0800

    Update run_assembly.c

--- run_assembly.c
+++ run_assembly.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void asm_main();
+//extern void asm_main();
 
 int main() {
     // Call the assembly function
-    asm_main();
+    //asm_main();
     
     printf("success");
         
