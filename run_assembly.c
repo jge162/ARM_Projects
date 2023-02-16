6338a21a39229f8ac0329f7e28fca961ab85b1d9
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-16 00:25:31 -0800

    Update run_assembly.c

--- run_assembly.c
+++ run_assembly.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 
 // Declare the assembly functions
-extern void stop_flashing_led(void);
-extern void led_switch(void);
-extern void built_in_led(void);
+extern void stop_flashing_led();   // function declaration in C code
+
+asm_main() {
+    // Call the assembly functions
+    stop_flashing_led();   // call the function by its correct name
+    ...
+}
+//extern void led_switch(void);
+//extern void built_in_led(void);
 
 int main() {
     // Call the assembly functions
     stop_flashing_led();
-    led_switch();
-    built_in_led();
+    //led_switch();
+    //built_in_led();
     
     printf("All is good!\n");
 
